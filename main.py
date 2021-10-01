1bba60ce057a093a07e6504e73d1f043b43c2377
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2021-10-01 22:31:52 +0200

    asyncio

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+import asyncio
 import fcntl
 import io
 import logging
 import os
 import select
 import time
-from datetime import datetime, timedelta
-from typing import Tuple, Optional, Generic, TypeVar
+from typing import Tuple, Optional, Generic, TypeVar, List, Generator, AsyncGenerator, Iterable
 
-import pulsectl
+import pulsectl_asyncio
+from pulsectl import PulseSourceInfo, PulseStateEnum
 
 logger = logging.getLogger('main')
 
@@ -16,40 +17,59 @@ MIC_ACTIVE_LED_MODE = 0  # domyślny, pusty tryb
 
 DEBUG = os.environ.get('MIC_STATE_DEBUG', '').lower() in ('1', 't', 'true')
 
+CLIENT_NAME = "keyboard-mic-state"
 
-def main():
-    pulse = pulsectl.Pulse()
+
+async def generate_pulse_sources():
+    while True:
+        async with pulsectl_asyncio.PulseAsync(CLIENT_NAME) as pulse:
+            source_list: List[PulseSourceInfo] = await pulse.source_list()
+        yield source_list
+        await asyncio.sleep(0.1)
+
+
+async def main():
     with FocusTty('/dev/ttyACM0') as tty:
         focus = FocusClient(tty)
+        pulse_sources_stream = generate_pulse_sources()
         prev_mode: Cell[Optional[int]] = Cell(None)
         try:
             focus.reset()
-            main_loop(focus, prev_mode, pulse)
+            await main_loop(focus, prev_mode, pulse_sources_stream)
         finally:
             logger.info('exit')
             restore(focus, prev_mode.get())
 
 
-def main_loop(focus, prev_mode, pulse):
+async def main_loop(focus, prev_mode, pulse_sources_stream: AsyncGenerator[List[PulseSourceInfo], None]):
     prev_sink_open = False
-    while True:
-        sink_open = any(m for m in pulse.source_list() if m.state == pulsectl.PulseStateEnum.running and not m.mute)
+    async for source_list in pulse_sources_stream:
+        # noinspection PyUnresolvedReferences
+        sink_open = any(m.state == PulseStateEnum.running and not m.mute for m in source_list)
         if sink_open and not prev_sink_open:
-            logger.info('sink opened')
-            prev_mode.set(focus.get_led_mode())
-            focus.set_led_mode(MIC_ACTIVE_LED_MODE)
-            focus.set_led_at(MIC_MUTE_KEY, (250, 0, 0))
+            on_source_change(focus, prev_mode, sink_open)
 
         if not sink_open and prev_sink_open:
-            logger.info('sink closed')
-            restore(focus, prev_mode.get())
-            prev_mode.set(None)
+            on_source_change(focus, prev_mode, sink_open)
 
         prev_sink_open = sink_open
         time.sleep(0.1)
 
 
+def on_source_change(focus: 'FocusClient', prev_mode: 'Cell[Optional[int]]', source_open: bool):
+    if source_open:
+        logger.info('source opened')
+        prev_mode.set(focus.get_led_mode())
+        focus.set_led_mode(MIC_ACTIVE_LED_MODE)
+        focus.set_led_at(MIC_MUTE_KEY, (250, 0, 0))
+    else:
+        logger.info('source closed')
+        restore(focus, prev_mode.get())
+        prev_mode.set(None)
+
+
 def restore(focus: 'FocusClient', prev_mode: 'Optional[int]'):
+    logger.info("restore")
     if prev_mode is not None:
         focus.set_led_mode(prev_mode)
 
@@ -59,7 +79,7 @@ class FocusClient:
         self._tty = tty
 
     def reset(self):
-        self._tty.read()
+        self._tty.discard_pending_data()
         self.run_command(b'')
 
     def get_led_mode(self) -> int:
@@ -74,15 +94,11 @@ class FocusClient:
         self.run_command('led.at {} {} {} {}'.format(index, *rgb).encode('ascii'))
 
     def run_command(self, command: bytes):
-        self._tty.read()  # Upewnij się że nic nie wisi w buforze
         self._tty.writeline(command)
         return list(self._read_result())
 
     def _read_result(self):
-        deadline = datetime.now() + timedelta(seconds=5)
-        while True:
-            time_left = deadline - datetime.now()
-            line = self._tty.readline(timeout=time_left)
+        for line in self._tty.readlines():
             if line == b'.':
                 return
             elif line:
@@ -105,7 +121,7 @@ class FocusTty:
         self.close()
 
     def open(self):
-        fd = os.open(self._path, os.O_RDWR | os.O_NOCTTY)
+        fd = os.open(self._path, os.O_RDWR | os.O_NOCTTY | os.O_NONBLOCK)
         try:
             fcntl.ioctl(fd, TIOCEXCL)
             self._tty = io.FileIO(fd, "r+b")
@@ -118,25 +134,37 @@ class FocusTty:
     def writeline(self, line: bytes) -> None:
         line = line + b'\n'
         logger.debug('write: %r', line)
-        self._tty.write(line)
+        assert self._tty.write(line) == len(line)
         self._tty.flush()
 
-    def readline(self, timeout: timedelta = None) -> bytes:
-        raw_line = self._tty.readline()
-        if not raw_line:
-            logger.debug('waiting for data')
-            select.select([self._tty], [], [], timeout.total_seconds())
-            raw_line = self._tty.readline()
+    def readlines(self) -> Generator[bytes, None, None]:
+        chunks = readchunks(self._tty)
+        for line in splitter(chunks, b'\n'):
+            yield line.removesuffix(b'\r')
 
-        if not raw_line:
-            raise EmptyReadException()
+    def discard_pending_data(self):
+        data = self._tty.read()
+        if data:
+            logger.warning('unexpected data when starting: %r', data)
 
-        line = raw_line.removesuffix(b'\r\n')
-        logger.debug('read: %r', line)
-        return line
 
-    def read(self):
-        return self._tty.read()
+def readchunks(f: io.FileIO) -> Generator[bytes, None, None]:
+    while True:
+        new_data = f.read()
+        logger.debug('read: %r', new_data)
+        if new_data:
+            yield new_data
+        logger.debug('waiting for more data')
+        select.select([f.fileno()], [], [])
+
+
+def splitter(chunks: Iterable[bytes], separator: bytes) -> Generator[bytes, None, None]:
+    buff = b''
+    for chunk in chunks:
+        buff += chunk
+        *results, buff = buff.split(separator)
+        yield from results
+    yield buff
 
 
 TIOCEXCL = 0x540C
@@ -165,6 +193,6 @@ if __name__ == '__main__':
             format='%(asctime)-15s %(levelname)s %(message)s',
             level=logging.DEBUG if DEBUG else logging.INFO,
         )
-        main()
+        asyncio.run(main())
     except KeyboardInterrupt:
         pass
